04816ef1be018be82f5abf3017e703af707585e1
AltairDusk <altairdusk@gmail.com>
2013-08-03 16:52:23 -0400

    New WNx dev mode output, added both top3 and top5 low tier stats with ability to customize.
    Now has a standard output without low tier stats and WNx development
    output that includes them.  Two sets of low tier stats are now output,
    top 3 and top 5 by default but those can be set to custom top x values.

--- README.txt
+++ README.txt
@@ -1,15 +1,22 @@
 WotStatGrabber is a simple script for retrieving user statistics from the official World of Tanks API with output in an easy to use .csv format.  
 
-You must provide 2 arguments when calling WotStatGrabber: the input file and the output file.  You can optionally provide the number of top played low tier tanks to return in the low tier battles calculation with the --top_lt flag.
+You must provide 2 arguments when calling WotStatGrabber: the input file and the output file.  You can optionally provide a flag to indicate WNx development mode and the number of top played low tier tanks to return in the low tier battles calculation.
 
 Usage:
-python WotStatGrabber.py inputFilePath outputFilePath --top_lt #
+python WotStatGrabber.py inputFilePath outputFilePath --wn --top_lt1 # --top_lt2 #
 
 inputFilePath: Full pathname of the input file which contains the names of users stats will be retrieved for.  Each username should be on a separate line.
 outputFilePath: Full pathname of the output file which will be generated with the statistics in CSV format.
---top_lt: (Optional) Number of top played low tier tanks to include in the low tier battles calculation, if not provided the top 3 most played low tier tanks will be used.
+--wn: (Optional) WNx development mode. This will output additional information useful in development of the WNx statistic.
+--top_lt1: (Optional) Number of top played low tier tanks used to calculate games played in low tiers for the first output field. This only functions when used with the --wn flag. If not provided 3 is used.
+--top_lt2: (Optional) Number of top played low tier tanks used to calculate games played in low tiers for the second output field. This only functions when used with the --wn flag. If not provided 5 is used.
 
-Example:
-python WotStatGrabber.py C:\temp\names.txt C:\temp\stats.csv --top_lt 5
+Examples:
+Basic output:
+python WotStatGrabber.py C:\temp\names.txt C:\temp\stats.csv
+WNx development mode:
+python WotStatGrabber.py C:\temp\names.txt C:\temp\stats.csv --wn
+WNx development mode with custom top(x) low tiers:
+python WotStatGrabber.py C:\temp\names.txt C:\temp\stats.csv --wn --top_lt1 2 --top_lt_2 8
 
 
--- src/WotStatGrabber.py
+++ src/WotStatGrabber.py
@@ -24,7 +24,9 @@ USER_ID_REQ_LIMIT = '1'
 USER_ID_REQ_URL = 'http://api.worldoftanks.com/uc/accounts/api/1.1/?{params}'
 USER_STATS_REQ_URL = ('http://api.worldoftanks.com/uc/accounts/'
                       '{id}/api/1.9/?{params}')
-_top_low_tiers = 3
+_wn = False
+_top_low_tiers1 = 3
+_top_low_tiers2 = 5
 
 
 class Error(Exception):
@@ -159,16 +161,31 @@ def prepare_output_row(user_stats, user_id):
     stats_row['tot_def_points'] = def_points
     stats_row['avg_def_points'] = def_points / battles
 
-    avg_tier_info = calc_tier_info(user_stats, battles, top_low_tiers= _top_low_tiers)
+    avg_tier_info = calc_tier_info(user_stats, battles)
     stats_row['avg_tier'] = avg_tier_info['avg_tier']
     stats_row['vehicle_battles'] = avg_tier_info['vehicle_battles']
-    stats_row['battles_top_low_tiers'] = avg_tier_info['top_low_battles']
-    stats_row['sum_tier_top_low_tiers'] = avg_tier_info['sum_low_tiers']
+    
+    if _wn:        
+        tlb1 = ''.join(['battles_top_', str(_top_low_tiers1), '_low_tiers'])
+        slt1 = ''.join(['sum_tier_top_', str(_top_low_tiers1), '_low_tiers'])
+        wlt1 = ''.join(['weighted_sum_tier_top_', 
+                        str(_top_low_tiers1), '_low_tiers'])
+        tlb2 = ''.join(['battles_top_', str(_top_low_tiers2), '_low_tiers']) 
+        slt2 = ''.join(['sum_tier_top_', str(_top_low_tiers2), '_low_tiers'])
+        wlt2 = ''.join(['weighted_sum_tier_top_', 
+                        str(_top_low_tiers2), '_low_tiers'])
+        
+        stats_row[tlb1] = avg_tier_info['top_low_battles1']
+        stats_row[slt1] = avg_tier_info['sum_low_tiers1']
+        stats_row[wlt1] = avg_tier_info['weighted_sum_low_tier1']
+        stats_row[tlb2] = avg_tier_info['top_low_battles2']
+        stats_row[slt2] = avg_tier_info['sum_low_tiers2']
+        stats_row[wlt2] = avg_tier_info['weighted_sum_low_tier2']
 
     return stats_row
 
 
-def calc_tier_info(user_stats, battles, top_low_tiers=3):
+def calc_tier_info(user_stats, battles):
     """Calculates the average tier and other tier-based
     information given a set of user statistics
 
@@ -183,25 +200,57 @@ def calc_tier_info(user_stats, battles, top_low_tiers=3):
     tier_info['avg_tier'] = 0
     low_tiers = []
 
-    # Spin through all vehicles and calculate battle/tier info
+    # Spin through all vehicles and gather low tier stats
     for vehicle in user_stats['vehicles']:
         tier = vehicle['level']
         vehicle_battles = vehicle['battle_count']
         tier_info['vehicle_battles'] += vehicle_battles
         tier_info['avg_tier'] += (tier / battles) * vehicle_battles
         
-        if tier <= 3:
+        if _wn and tier <= 3:
             low_tiers.append([vehicle_battles, tier])
-            
-    low_battles = 0
-    tier_sum = 0
-    for vehicle in sorted(low_tiers, key=itemgetter(0), reverse=True)[:top_low_tiers]:
-        low_battles += vehicle[0]
-        tier_sum += vehicle[1]
-    tier_info['top_low_battles'] = low_battles
-    tier_info['sum_low_tiers'] = tier_sum
+    
+    if _wn:        
+        # Calculate for the first set of top low tiers
+        low_tier_info1 = calc_low_tiers(low_tiers, _top_low_tiers1)
+        tier_info['top_low_battles1'] = low_tier_info1['top_low_battles']
+        tier_info['sum_low_tiers1'] = low_tier_info1['sum_low_tier']
+        tier_info['weighted_sum_low_tier1'] = low_tier_info1['weighted_low_tier']
+        
+        # Calculate for the second set of top low tiers
+        low_tier_info = calc_low_tiers(low_tiers, _top_low_tiers2)
+        tier_info['top_low_battles2'] = low_tier_info['top_low_battles']
+        tier_info['sum_low_tiers2'] = low_tier_info['sum_low_tier']
+        tier_info['weighted_sum_low_tier2'] = low_tier_info['weighted_low_tier']
     
     return tier_info
+
+
+def calc_low_tiers(low_tiers, top_x):
+    """Calculates battles, tier sum, and weighted tier in top_x low_tiers 
+    given a list of low_tier tank stats
+    
+    Keyword arguments:
+        low_tiers -- low tier tank statistics
+        top_x -- number of top low tier tanks to calculate for
+        
+    """
+    
+    low_tier_info = {}
+    low_battles = 0
+    tier_sum = 0
+    weighted_tier = 0
+    for vehicle in sorted(low_tiers, key=itemgetter(0), reverse=True)[:top_x]:
+        low_battles += int(vehicle[0])
+        tier_sum += int(vehicle[1])
+        weighted_tier += (int(vehicle[1]) * int(vehicle[0]))
+    weighted_tier = weighted_tier * _top_low_tiers1 / low_battles
+    
+    low_tier_info['top_low_battles'] = low_battles
+    low_tier_info['sum_low_tier'] = tier_sum
+    low_tier_info['weighted_low_tier'] = weighted_tier
+    
+    return low_tier_info
     
     
 def create_user_stats_csv(input_file, output_file):
@@ -213,7 +262,6 @@ def create_user_stats_csv(input_file, output_file):
         output_file -- CSV file that will be created with results
         
     """
-    
     fieldnames = [
         'name', 'id', 'battles',
         'tot_xp', 'avg_xp', 'tot_spots', 
@@ -221,8 +269,22 @@ def create_user_stats_csv(input_file, output_file):
         'tot_dmg', 'avg_dmg', 'wins',
         'win_pct', 'tot_cap_points', 'avg_cap_points',
         'tot_def_points', 'avg_def_points', 'avg_tier', 
-        'vehicle_battles', 'battles_top_low_tiers', 'sum_tier_top_low_tiers'
+        'vehicle_battles'
         ]
+    
+    if _wn:
+        fieldnames.extend([
+            ''.join(['battles_top_', str(_top_low_tiers1), '_low_tiers']), 
+            ''.join(['sum_tier_top_', str(_top_low_tiers1), '_low_tiers']),
+            ''.join(['weighted_sum_tier_top_', 
+                     str(_top_low_tiers1), '_low_tiers'
+                     ]),
+            ''.join(['battles_top_', str(_top_low_tiers2), '_low_tiers']), 
+            ''.join(['sum_tier_top_', str(_top_low_tiers2), '_low_tiers']),
+            ''.join(['weighted_sum_tier_top_', 
+                     str(_top_low_tiers2), '_low_tiers'
+                     ])
+            ])
     
     with open(input_file, encoding='utf-8') as names_file:
         with open(output_file, 'w', newline='') as csv_file:
@@ -247,19 +309,43 @@ def create_user_stats_csv(input_file, output_file):
 
 if __name__ == '__main__':
     # Parse the arguments
-    parser = argparse.ArgumentParser(description = 'Retrieve and output WoT User Statistics.')
+    parser = argparse.ArgumentParser(description = '''Retrieve and output WoT 
+                                                    User Statistics.'''
+                                    )
     parser.add_argument('input_file', metavar='input_file', type=str, 
-                        help='Complete path to a file containing the names to retrieve stats for (one name per line).'
+                        help='''Complete path to a file containing the names to 
+                                retrieve stats for (one name per line).'''
                         )
     parser.add_argument('output_file', metavar='output_file', type=str,
-                        help='Complete path for the output file containing stats (will be overwritten if it exists).'
+                        help='''Complete path for the output file containing 
+                                stats (will be overwritten if it exists).'''
                         )
-    parser.add_argument('--top_lt', metavar='top_lt', type=int,
-                        help='(Optional) Number of top played low tier tanks used to calculate games played in low tiers.')
+    parser.add_argument('--wn', action='store_true',
+                        help='''(Optional) WNx development mode. This will 
+                                output additional information useful in 
+                                development of the WNx statistic.'''
+                        )
+    parser.add_argument('--top_lt1', metavar='N', type=int,
+                        help='''(Optional) Number of top played low tier tanks 
+                                used to calculate games played in low tiers 
+                                for the first output field. This only 
+                                functions when used with the --wn flag.'''
+                        )
+    parser.add_argument('--top_lt2', metavar='N', type=int,
+                        help='''(Optional) Number of top played low tier tanks 
+                                used to calculate games played in low tiers 
+                                for the second output field. This only 
+                                functions when used with the --wn flag.'''
+                        )
     args = parser.parse_args()
     
-    if args.top_lt:
-        _top_low_tiers = args.top_lt
+    if args.wn:
+        _wn = True
+        
+        if args.top_lt1:
+            _top_low_tiers1 = args.top_lt1
+        if args.top_lt2:
+            _top_low_tiers2 = args.top_lt2
     
     # Get the stats
     create_user_stats_csv(args.input_file, args.output_file)
